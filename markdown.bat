d4cbef115e3523e43431ad07e3e9863b92df5300
Jonathan Gonse <jonathan.gonse@gmail.com>
2011-12-04 22:44:19 +0800

    remove rtf output and fix font and text-indent in css for Chinese display.

--- markdown.bat
+++ markdown.bat
@@ -3,8 +3,6 @@ pandoc -s %1 -t plain -o %1.txt
 pandoc -s -S --toc -c pandoc.css -A footer.html %1 -o %1-toc.html
 %1-toc.html
 
-pandoc -s -S -c pandoc.css -A footer.html %1 -o %1.html
-
-pandoc -s %1 -t rtf -o %1.rtf 
+pandoc -s -S %1 -o %1.html
 
 exit
