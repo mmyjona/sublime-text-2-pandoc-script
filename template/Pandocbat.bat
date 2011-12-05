2f045b45ff2543c315a0e746d1f52d3ff1e02f44
Jonathan Gonse <jonathan.gonse@gmail.com>
2011-12-05 14:32:44 +0800

    fix automatic build detection

--- template/Pandocbat.bat
+++ template/Pandocbat.bat
@@ -1,8 +1,14 @@
-pandoc -s %1 -t plain -o %1.txt 
+pandoc %1 -t plain -o %1.txt 
 
-pandoc -s -S --toc -c pandoc.css -A footer.html %1 -o %1-toc.html
+pandoc -S -5 --toc -c pandoc.css -A footer.html %1 -o %1-toc.html
 %1-toc.html
 
-pandoc -s -S %1 -o %1.html
+pandoc -S %1 -o %1.html
+
+pandoc -S %1 -o %1.html
+
+pandoc  --offline -w s5 -s %1 > %1-s5.html
+
+pandoc  --offline -w slidy -s %1 > %1-slidy.html
 
 exit
