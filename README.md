bf2b1858c287c3e243ce1ab07f8f543db1f2c7b2
Jonathan Gonse <jonathan.gonse@gmail.com>
2011-12-12 00:24:05 +0800

    update iconv to solve encoding

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Pandoc building batch file for sublime text 2.
 
 * install [pandoc](https://code.google.com/p/pandoc/downloads/list). You may need to put pandoc.exe in your PATH.
+* install [iconv for win](http://gnuwin32.sourceforge.net/packages/libiconv.htm) to solve encoding issue.(optional)
 * get [sublime text 2](http://www.sublimetext.com/2).
 * put all files under d:\Sublime text 2\Data\Packages\User\ and copy files in template folder to the folder where your pandoc file is.
 * put other files within the same folder as the pandoc doc.
--- template/Pandocbat.bat
+++ template/Pandocbat.bat
@@ -1,14 +1,12 @@
-pandoc %1 -t plain -o %1.txt 
+pandoc %1  -t plain | iconv -f utf-8 | iconv -t utf-8 > %1.txt
 
-pandoc -S -5 --toc -c pandoc.css -A footer.html %1 -o %1-toc.html
+pandoc -S -5 --toc -c pandoc.css -A footer.html %1 | iconv -f utf-8 | iconv -t utf-8 > %1-toc.html
 %1-toc.html
 
-pandoc -S %1 -o %1.html
+pandoc -S %1 | iconv -f utf-8 | iconv -t utf-8 > %1.html
 
-pandoc -S %1 -o %1.html
+pandoc  --offline -w s5 -s %1 | iconv -f utf-8 | iconv -t utf-8 > %1-s5.html
 
-pandoc  --offline -w s5 -s %1 > %1-s5.html
-
-pandoc  --offline -w slidy -s %1 > %1-slidy.html
+pandoc  --offline -w slidy -s %1 | iconv -f utf-8 | iconv -t utf-8 > %1-slidy.html
 
 exit
